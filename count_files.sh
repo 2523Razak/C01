b9a622be5b601fe0ad950e4c0aa1cb9f112140c9
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-12 16:53:53 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
-# Ce script compte le nombre de fichiers dans un dossier donné
+# Script pour compter le nombre de fichiers dans un dossier
 
 echo "Entrez le nom du dossier :"
 read dossier
-
-# Vérifie si le dossier existe
 if [ -d "$dossier" ]; then
-    nb_fichiers=$(ls -p "$dossier" | grep -v / | wc -l)
-    echo "Le dossier '$dossier' contient $nb_fichiers fichier(s)."
+  nb=$(ls -p "$dossier" | grep -v / | wc -l)
+  echo "Le dossier '$dossier' contient $nb fichier(s)."
 else
-    echo "Le dossier '$dossier' n'existe pas."
+  echo "Le dossier '$dossier' n'existe pas."
 fi
