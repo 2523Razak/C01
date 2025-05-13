575183aeb29c3ff0a77056161a0199bcde5b88e1
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-13 01:33:02 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # Exercice 1 - Comptage simple avec ls et wc -l
-
 echo "Entrez le chemin du répertoire :"
 read repertoire
-
 if [ -d "$repertoire" ]; then
   nb=$(ls "$repertoire" | wc -l)
   echo "Il y a $nb éléments dans le répertoire."
