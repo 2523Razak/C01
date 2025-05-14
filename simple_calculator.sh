c289a30b96f4500638fab8f434ae84e67d4131ad
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 21:01:32 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
+# On lit deux nombres et un opérateur 
 read a b op
-
+# Si l’opérateur est +
 if [ "$op" = "+" ]; then
 echo "Résultat : $((a + b))"
 fi
-
+# Si l’opérateur est -
 if [ "$op" = "-" ]; then
 echo "Résultat : $((a - b))"
 fi
-
+# Si l’opérateur est *
 if [ "$op" = "*" ]; then
 echo "Résultat : $((a * b))"
 fi
-
+# Si l’opérateur est /
 if [ "$op" = "/" ]; then
 echo "Résultat : $((a / b))"
 fi
