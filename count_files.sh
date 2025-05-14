0c678a2f090c3549845f666d06f3781149c08705
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 10:09:55 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo -n "Le dossier $1 contient "
-ls -1 "$1" | wc -l | tr -d ' '
-echo " fichier(s)."
+nombre_fichiers=$(ls | wc -l)
+echo "Le dossier contient $nombre_fichiers élément(s)."
