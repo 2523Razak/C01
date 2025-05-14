acae0342ef4d5a5f5de277faa0b9eb1c9e1709c2
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 09:09:30 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-# Je liste tous les fichiers dans le dossier actuel avec ls
-# Puis je compte combien il y en a avec wc -l
-nombre_fichiers=$(ls | wc -l)
 
-# J'affiche le résultat comme dans l'exemple
-echo "Le dossier actuel contient $nombre_fichiers fichier(s)."
+echo -n "Le dossier $1 contient "
+ls -1 "$1" | wc -l | tr -d ' '
+echo " fichier(s)."
