1d9c2f5df7069d7ae7aac48562e54a27583b62b9
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 21:21:01 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+# Lire le nom du fichier
 read fichier
+# Lire la chaîne à rechercher
 read chaine
+# Si la chaîne est trouvée
 if grep "$chaine" "$fichier"; then
 echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
+# Si la chaîne n'est pas
 echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
