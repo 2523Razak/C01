e4893ed6e4e563b0a969a20fc610975c8c93790a
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 20:50:18 +0100

    Update simple_calculator.sh

--- simple_calculator.sh
+++ simple_calculator.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
+read a
+read op
+read b
 
+if [ "$op" = "+" ]; then
+  echo "Résultat : $((a + b))"
+elif [ "$op" = "-" ]; then
+  echo "Résultat : $((a - b))"
+elif [ "$op" = "*" ]; then
+  echo "Résultat : $((a * b))"
+elif [ "$op" = "/" ]; then
+  echo "Résultat : $((a / b))"
+else
+  echo "Opérateur non reconnu."
+fi
