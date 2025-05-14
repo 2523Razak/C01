5f20762eb9cb7d4de841404f6635038071dd159f
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 14:11:20 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 # exercice 2 find_string.sh
-
-echo "Entrez le nom du fichier :"
 read fichier
-
-echo "Entrez le mot à rechercher :"
 read mot
-
 # je cherche le mot et je teste s'il a été trouvé
 if grep -q "$mot" "$fichier"; then
   echo "La chaîne'$mot'a été trouvée dans $fichier."
