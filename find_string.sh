cdb8ac8113871c3c6655e6cdbc5448f6fc22e281
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 14:34:27 +0100

    Update find_string.sh

--- find_string.sh
+++ find_string.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 read fichier
 read chaine
-if grep -q "$chaine" "$fichier"; then
-  echo "La chaîne'$chaine'a été trouvée dans $fichier."
+if grep "$chaine" "$fichier"; then
+echo "La chaîne '$chaine' a été trouvée dans $fichier."
 else
-  echo "La chaîne'$chaine'n'a pas été trouvée dans $fichier."
+echo "La chaîne '$chaine' n'a pas été trouvée dans $fichier."
 fi
