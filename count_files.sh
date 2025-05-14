786e527c2ba15c44a5656b09911c4ba0b9a40735
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 21:17:13 +0100

    Update count_files.sh

--- count_files.sh
+++ count_files.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Lire le nom du dossier saisi par l'utilisateur
 read dossier
+# Compter le nombre d’éléments
 nb=$(ls "$dossier" | wc -l)
+# Afficher le résultat
 echo "Le dossier $dossier contient $nb fichier(s)."
