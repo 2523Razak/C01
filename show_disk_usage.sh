c00188f7e0f62ecc9ecd3898ac2f3349a5cadf9a
2523Razak <68425641+2523Razak@users.noreply.github.com>
2025-05-14 21:24:37 +0100

    Update show_disk_usage.sh

--- show_disk_usage.sh
+++ show_disk_usage.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# Afficher l’utilisation du disque de façon lisible
 df -h
+# Demander à l’utilisateur de saisir le nom d’un dossier
 echo "Entrez le nom d’un dossier pour voir son poids :"
 read dossier
+# Vérifier si le dossier existe
 if [ -d "$dossier" ]; then
-du -sh "$dossier"
+du -sh "$dossier" # Afficher la taille du
 else
+# Afficher un message si le dossier n’existe pas
 echo "Le dossier '$dossier' n'existe pas."
 fi
